70dfc9790a959c6eef8dfa988436dbc97a7f5ae5
Peter Marcelis <peter.marcelis@hva.nl>
2018-11-05 09:57:30 +0100

    Added skyboxes & lighting changes

---
+++ SpaarneVRPrototype/Assets/Scripts/RotateLeft.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLeft : MonoBehaviour {
+	
+	public float speed = 30f;
+	
+	public List<GameObject> Objects;
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(GameObject g in Objects)
+		g.transform.RotateAround(g.transform.position, g.transform.up, Time.deltaTime * speed);
+	}
+}
