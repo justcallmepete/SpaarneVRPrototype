4d54bda34a9999af5f1339bca0554a291afbbd21
Unknown <Rick.nl@hotmail.com>
2018-11-12 10:54:17 +0100

    Fixed warning system

--- SpaarneVRPrototype/Assets/Scripts/WarningSystem.cs
+++ SpaarneVRPrototype/Assets/Scripts/WarningSystem.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,8 +7,29 @@ using UnityEngine.UI;
 public class WarningSystem : MonoBehaviour {
     public GameObject warningHolder;
     public Text warningText;
+    public GameObject Holder1;
+    public GameObject Holder2;
+    [SerializeField]
     private float Timer;
     private float fadeCol;
+
+    private void Start()
+    {
+        if (Holder1.activeInHierarchy)
+        {
+            warningHolder = Holder1;
+            warningText = Holder1.transform.GetChild(0).transform.GetComponent<Text>();
+        }
+        else if(Holder2.activeInHierarchy){
+            warningHolder = Holder2;
+            warningText = Holder2.transform.GetChild(0).transform.GetComponent<Text>();
+
+        }
+        Timer = 0;
+        warningText.text = "test";
+        Timer = 0f;
+        fadeCol = 1f;
+    }
 
     private void Update()
     {
