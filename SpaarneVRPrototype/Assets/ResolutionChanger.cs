3309fbbb51da642d937b4aeb9eab7c5882dbf3b8
Peter Marcelis <peter.marcelis@hva.nl>
2018-11-17 14:35:25 +0100

    test

---
+++ SpaarneVRPrototype/Assets/ResolutionChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        Debug.Log("Set to: " + UnityEngine.XR.XRSettings.eyeTextureResolutionScale);
+        UnityEngine.XR.XRSettings.eyeTextureResolutionScale = 1.5f;
+        OVRManager.tiledMultiResLevel = OVRManager.TiledMultiResLevel.LMSMedium; // LMSHigh
+     //   OVRManager.display.displayFrequency = 72.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
