abdf3392ad5615df000dd307071961f7832ebf03
Peter Marcelis <peter.marcelis@hva.nl>
2018-12-03 15:23:10 +0100

    added score system beta version

---
+++ SpaarneVRPrototype/Assets/Scripts/Scriptable Object/FailureCounter.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "failureCounter", menuName = "Spaarne/Scriptable/counter")]
+public class FailureCounter : ScriptableObject
+{
+    public int counter = 0;
+}
