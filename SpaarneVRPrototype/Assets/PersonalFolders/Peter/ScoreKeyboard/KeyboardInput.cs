d8655f78f62e275bab1d3642e91c606f4dbfde70
Peter Marcelis <peter.marcelis@hva.nl>
2018-12-28 12:48:23 +0100

    Added Keyboard
    Created A User Interface for entering your name after finishing the procedure

---
+++ SpaarneVRPrototype/Assets/PersonalFolders/Peter/ScoreKeyboard/KeyboardInput.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardInput : MonoBehaviour {
+
+	public List<Button> buttons = new List<Button>();
+
+	string PlayerName;
+	[SerializeField]
+	private Text text;
+	private bool firstPress = false;
+
+	private void Awake()
+	{
+		foreach(Button b in buttons)
+		{
+			b.onClick.AddListener(delegate { ButtonPress(b.name); });
+		}
+	}
+
+	void ButtonPress(string button)
+	{
+		if (!firstPress)
+		{
+			text.text = "";
+			firstPress = true;
+		}
+
+		if(button == "Backspace")
+		{
+			if (PlayerName.Length <= 0) return;
+
+			PlayerName = PlayerName.Substring(0, PlayerName.Length - 1);
+		} else
+		if (button == "Enter")
+		{
+			// ToDo: Submit Score to List
+		}else
+		if (button == "Space")
+		{
+			PlayerName += " ";
+		}else
+		{
+			PlayerName += button;
+		}
+		text.text = PlayerName;
+	}
+
+}
