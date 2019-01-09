44fbd21d7d847ee3af2d39818fd104296a882ad2
Unknown <robinburggraaf92@hotmail.com>
2019-01-09 13:51:03 +0100

    Sound added shower/quest

---
+++ SpaarneVRPrototype/Assets/Scripts/QuestSoundCompletion.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestSoundCompletion : MonoBehaviour
+{
+    public GameObject questOne;
+    public GameObject questTwo;
+    public AudioClip questSound;
+    public AudioSource VRAudioPlayer;
+    public AudioSource ComputerAudioPlayer;
+    public AudioSource activeAudioPlayer;
+    public List<bool> soundPlayed = new List<bool>();
+    public List<bool> questSteps = new List<bool>();
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		if (questTwo.activeInHierarchy)
+        {
+         questSteps= questTwo.GetComponent<QuestTwo>().questStepts;
+        }
+        else
+        {
+            questSteps = questOne.GetComponent<QuestOne>().questSteps;
+           
+        }
+       // Debug.Log(questSteps.Count);
+     
+        if (VRAudioPlayer.isActiveAndEnabled)
+        {
+            activeAudioPlayer = VRAudioPlayer;
+        }
+        else
+        {
+            activeAudioPlayer = ComputerAudioPlayer;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (soundPlayed.Count == 0) {
+            for (int i = 0; i < questSteps.Count; i++)
+            {
+                soundPlayed.Add(false);
+            }
+        }
+        else
+        {
+
+        
+
+        for (int i = 0; i < questSteps.Count; i++)
+        {
+            if (questSteps[i] && !soundPlayed[i])
+            {
+                activeAudioPlayer.clip = questSound;
+                Debug.Log(activeAudioPlayer.clip);
+                activeAudioPlayer.Play();
+                Debug.Log(activeAudioPlayer.isPlaying);
+                soundPlayed[i] = true;
+            }
+        }
+        }
+    }
+}
---
+++ SpaarneVRPrototype/Assets/Scripts/ShowerSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowerSound : MonoBehaviour {
+    public GameObject Shower_Sound;
+    public PersonPosition personPosition;
+    public InteractionDoor interactionDoor;
+
+
+	// Update is called once per frame
+	void Update () {
+
+
+        if (personPosition.inTube && interactionDoor.doorIsOpen || personPosition.inRoom)
+        {
+            Shower_Sound.SetActive(true);
+        }
+        else {
+            Shower_Sound.SetActive(false);
+        }
+
+
+    }
+}
