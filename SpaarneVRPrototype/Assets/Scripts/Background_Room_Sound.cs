a777aa17d82f0b7b5c39856205098f273d0f1916
Unknown <robinburggraaf92@hotmail.com>
2019-01-11 21:26:53 +0100

    Hallway / interaction sounds added

---
+++ SpaarneVRPrototype/Assets/Scripts/Background_Room_Sound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Background_Room_Sound : MonoBehaviour {
+
+    //public AudioClip backgroundSound;
+    public AudioSource backgroundSource;
+    public PersonPosition personPosition;
+    public InteractionDoor interactionDoor;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (personPosition.inTube && !interactionDoor.doorIsOpen || personPosition.inRoom)
+        {
+            backgroundSource.volume = 0;
+        }
+        else
+        {
+            backgroundSource.volume = 1;
+        }
+
+
+    }
+}
